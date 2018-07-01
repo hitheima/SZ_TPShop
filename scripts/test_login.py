3340a90189668333f676b7927d1bd1e486a05e03
hitheima <hitheima@163.com>
2018-07-01 19:33:40 +0800

    通过toast判断登录是否成功。

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -7,9 +7,7 @@ class TestLogin:
         self.driver = init_driver()
         self.page = Page(self.driver)
 
-    def test_login1(self):
-        print("hehe")
-
+    def test_login(self):
         # 主页点击我的
         self.page.home.click_mine()
         # 我的点击登录
@@ -20,5 +18,8 @@ class TestLogin:
         self.page.login.input_password("123000")
         # 登录页面点击登录
         self.page.login.click_login()
+
+        # self.page.login.is_toast_exist("登录成功")
+        assert "登录成功" == self.page.login.find_toast("登录成功")
 
 
