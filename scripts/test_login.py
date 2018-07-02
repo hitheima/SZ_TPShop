8fe27fbe5959aa894f1d2d522b59581f9fc1ecbf
hitheima <hitheima@163.com>
2018-07-02 15:37:00 +0800

    显示密码功能的参数化。

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,9 +1,30 @@
+import random
+
 import pytest
 from selenium.webdriver.common.by import By
 
 from base import init_driver
 from page import Page
 from base import analyze_data
+
+
+def random_password():
+    """
+    随机生成一个8位的数字的字符串
+    :return:
+    """
+    password = ""
+    for i in range(8):
+        password += str(random.randint(0, 9))
+    return password
+
+def show_password_data():
+    data_list = list()
+    data_list.append(random_password())
+    data_list.append(random_password())
+    data_list.append(random_password())
+    return data_list
+
 
 
 class TestLogin:
@@ -47,8 +68,10 @@ class TestLogin:
     #         self.page.login.input_password(password)
     #     assert not self.page.login.is_login_button_enabled()
 
-    def test_show_password(self):
-        password = "xxxx"
+
+    @pytest.mark.parametrize("password", show_password_data())
+    def test_show_password(self, password):
+        # password = args
 
         # 主页点击我的
         self.page.home.click_mine()
@@ -56,11 +79,37 @@ class TestLogin:
         self.page.mine.click_login_signup()
         # 输入密码
         self.page.login.input_password(password)
-        # 点击显示密码
-        self.page.login.click_show_password()
 
-        # 判断当初输入的密码是否存在
-        assert self.page.login.is_feature_exist((By.XPATH, "text," + password))
+        # if 真        真
+        # if 假        假
+
+        if self.page.login.is_feature_exist((By.XPATH, "text," + password)):
+            assert False
+        else:
+            # 点击显示密码
+            self.page.login.click_show_password()
+            # 判断当初输入的密码是否存在
+            assert self.page.login.is_feature_exist((By.XPATH, "text," + password))
+
+
+        # # if 真 == 假       假
+        # # if 假 == 假       真
+        #
+        # # if 真       假
+        # # if 假       真
+
+        #
+        # if not self.page.login.is_feature_exist((By.XPATH, "text," + password)):
+        #     # 点击显示密码
+        #     self.page.login.click_show_password()
+        #     # 判断当初输入的密码是否存在
+        #     assert self.page.login.is_feature_exist((By.XPATH, "text," + password))
+        # else:
+        #     assert False
+
+
+
+
 
 
         # try:
